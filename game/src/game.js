a8297c3b4a21fcbbe1540e5f73c9fa7a7011a5b7
LexSava <lexsava@gmail.com>
2021-01-11 12:52:22 +0300

    feat: add class ImageLoader
    Add class ImageLoader and connected it.

--- game/src/game.js
+++ game/src/game.js
@@ -2,8 +2,15 @@ import { Screen } from './screen';
 import { Loading } from './scenes/loading';
 
 export class Game {
-    constructor() {
-        this.screen = new Screen(480,480);
+    constructor({ width = 640, height = 640 } = {}) {
+
+        this.screen = new Screen(width, height);
+        this.screen.loadImages({
+            orc: 'img/orc.png',
+            player: 'img/player.png',
+            title: 'img/title.jpg',
+            tiles: 'img/tiles.png'
+        });
         this.scenes = {
             loading: new Loading(this)
         };
@@ -12,7 +19,7 @@ export class Game {
     }
 
     frame(time) {
-        if(this.currentScene.isActive == false) {
+        if (this.currentScene.isActive == false) {
             this.currentScene = this.scenes[this.currentScene.nextScene];
             this.currentScene.init();
         }
