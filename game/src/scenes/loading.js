5d2b344758cf8e91d13148d67f9d6d75035257af
LexSava <lexsava@gmail.com>
2021-01-11 11:35:39 +0300

    feat: add class Loading for scene
    Add class Loading for scene

---
+++ game/src/scenes/loading.js
@@ -0,0 +1,10 @@
+import { Scene } from '../scene';
+
+export class Loading extends Scene {
+    constructor(game) {
+        super(game);
+    }
+    render(time) {
+        super.render(time);
+    }
+}
