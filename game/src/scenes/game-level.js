e0e84201cbe8c81f808f9782d2417d34fe53c10e
LexSava <lexsava@gmail.com>
2021-01-12 12:54:53 +0300

    feat: (test) rendered the sprite on the page
    (test) Rendered the sprite on the page.

--- game/src/scenes/game-level.js
+++ game/src/scenes/game-level.js
@@ -1,8 +1,16 @@
 import { Scene } from '../scene';
+import { SpriteSheet } from '../sprite-sheet';
 
 export class GameLevel extends Scene {
     constructor(game) {
         super(game);
+        this.tiles = new SpriteSheet({
+            imageName: 'tiles',
+            imageWidth: 640,
+            imageHeight: 640
+        });
+        this.tree = this.tiles.getSprite(7);
+        this.tree.setXY(10, 10);
     }
 
     init() {
@@ -11,6 +19,7 @@ export class GameLevel extends Scene {
 
     render(time) {
         this.game.screen.fill('#000000');
+        this.game.screen.drawSprite(this.tree);
         super.render(time);
     }
 }
