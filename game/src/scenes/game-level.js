aa9840da43cb40fcff609f1820784920e66b56e9
LexSava <lexsava@gmail.com>
2021-01-12 19:31:44 +0300

    feat: testing animation work
    Testing animation work.

--- game/src/scenes/game-level.js
+++ game/src/scenes/game-level.js
@@ -11,15 +11,26 @@ export class GameLevel extends Scene {
         });
         this.tree = this.tiles.getSprite(7);
         this.tree.setXY(10, 10);
+        this.orcTiles = new SpriteSheet({
+            imageName: 'orc',
+            imageWidth: 832,
+            imageHeight: 1344
+        });
+        this.orc = this.orcTiles.getAnimation([1, 2, 3, 4, 5, 6, 7], 300);
     }
 
     init() {
         super.init();
     }
 
+    update(time) {
+        this.orc.update(time);
+    }
     render(time) {
+        this.update(time);
         this.game.screen.fill('#000000');
         this.game.screen.drawSprite(this.tree);
+        this.game.screen.drawSprite(this.orc);
         super.render(time);
     }
 }
